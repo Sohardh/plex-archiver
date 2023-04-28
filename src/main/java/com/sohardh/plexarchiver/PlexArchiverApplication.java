c60be675f657b43253535fe8cd112f23398bab10
Sohardh Chobera <sohardhchobera684@gmail.com>
2023-04-29 00:16:39 +0530

    initial commit

---
+++ src/main/java/com/sohardh/plexarchiver/PlexArchiverApplication.java
@@ -0,0 +1,13 @@
+package com.sohardh.plexarchiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlexArchiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlexArchiverApplication.class, args);
+	}
+
+}
